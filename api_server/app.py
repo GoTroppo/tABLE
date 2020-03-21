c51ebca62a397acdaf9c04c3353ce9c9b9971b62
Geek Boy <github@geekboy.com.au>
2020-03-21 14:52:08 +1000

    Converted from neopixel library to rpi_ws281x as could not control brightness

--- api_server/app.py
+++ api_server/app.py
@@ -4,19 +4,29 @@ from flask import Flask, jsonify, abort, make_response
 from flask import request
 
 import time
-import board
+#import board
 import neopixel
+from rpi_ws281x import *
 
-flask_port=5000
-init_brightness = 0.2
-#full_brightness_perc = 0.5
+# LED strip configuration:
+PIXEL_COUNT = 24      # Number of LED pixels.
 
 # Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
 # NeoPixels must be connected to D10, D12, D18 or D21 to work.
-pixel_pin = board.D18
+#PIXEL_PIN = board.D18      # GPIO pin connected to the pixels (must support PWM!).
+# Needed to change the 
+PIXEL_PIN = 18      # GPIO pin connected to the pixels (must support PWM!).
+
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10       # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 100  # Set to 0 for darkest and 255 for brightest
+# True to invert the signal (when using NPN transistor level shift)
+LED_INVERT = False
+
+FLASK_PORT=5000
 
 # The number of NeoPixels
-num_pixels = 24
+NUM_PIXELS = 24
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
@@ -24,66 +34,101 @@ num_pixels = 24
 # Order is reversed for Duinotech RGB LED Ring so use GRB
 ORDER = neopixel.GRB
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=init_brightness, auto_write=False,
-                           pixel_order=ORDER)
+# Create NeoPixel object with appropriate configuration.
+pixel_strip = Adafruit_NeoPixel(NUM_PIXELS, PIXEL_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+pixel_strip.begin()
 
 def set_pixel(pixel_index: int, pixel_colour: str, set_single: bool = True):
     if set_single == 1 :
         # Clear all pixels
-        pixels.fill((0, 0, 0))
+        blank_neopixel()
 
-    pixels[pixel_index] = hex_to_rgb(pixel_colour)
-    pixels.show()
+    pixel_color_tup = hex_to_rgb(pixel_colour)
+    pixel_strip.setPixelColor(pixel_index, Color(pixel_color_tup[0], pixel_color_tup[1], pixel_color_tup[2]))    
+
+    pixel_strip.show()
 
 def blank_neopixel():
-    pixels.fill((0, 0, 0))
-    pixels.show()
+    for i in range(0, pixel_strip.numPixels(), 1):
+        pixel_strip.setPixelColor(i, Color(0, 0, 0))
+    pixel_strip.show()
 
 def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        r = g = b = 0
-    elif pos < 85:
-        r = int(pos * 3)
-        g = int(255 - pos*3)
-        b = 0
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
     elif pos < 170:
         pos -= 85
-        r = int(255 - pos*3)
-        g = 0
-        b = int(pos*3)
+        return Color(255 - pos * 3, 0, pos * 3)
     else:
         pos -= 170
-        r = 0
-        g = int(pos*3)
-        b = int(255 - pos*3)
-    return (r, g, b) if ORDER == neopixel.RGB or ORDER == neopixel.GRB else (r, g, b, 0)
+        return Color(0, pos * 3, 255 - pos * 3)
 
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            pixel_index = (i * 256 // num_pixels) + j
-            pixels[i] = wheel(pixel_index & 255)
-        pixels.show()
-        time.sleep(wait)
+def rainbow(strip, wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i+j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
 
+def rainbowCycle(strip, wait_ms=20, iterations=5):
+    """Draw rainbow that uniformly distributes itself across all pixels."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def theaterChaseRainbow(strip, wait_ms=50, chase_range=256):
+    """Rainbow movie theater light style chaser animation."""
+    for j in range(chase_range):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, wheel((i+j) % 255))
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
 
 def hex_to_rgb(value):
     """Return (red, green, blue) for the color given as hex form for rrggbb."""
-    #value = value.lstrip('#')
     lv = len(value)
     return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
 
 app = Flask(__name__)
 
-@app.route('/rainbow', methods=['GET'])
-def rainbow():
-    rainbow_cycle(0.0051)
+@app.route('/rainbow_cycle', methods=['GET'])
+def do_rainbow_cycle():
+    rainbowCycle(pixel_strip,20,1)
     time.sleep(1.0)
     blank_neopixel()
-    return jsonify({'rainbow': 'Pretty'})
+    return jsonify({'rainbow_cycle': True})
+
+@app.route('/rainbow_chase', methods=['GET'])
+def do_rainbow_chase():
+    theaterChaseRainbow(pixel_strip,20,256)
+    time.sleep(1.0)
+    blank_neopixel()
+    return jsonify({'rainbow_chase': True})
+
+@app.route('/rainbow', methods=['GET'])
+def do_rainbow():
+    rainbow(pixel_strip)
+    blank_neopixel()
+    return jsonify({'rainbow': True})
+
+@app.route('/brightness/<int:brightness>', methods=['GET'])
+def set_pixel_strip_brightness(brightness):
+    pixel_strip.setBrightness(brightness)
+    pixel_strip.show()
+    return jsonify({'brightness': brightness})
+
+@app.route('/clear', methods=['GET'])
+def clear_neopixel():
+    blank_neopixel()
+    return jsonify({'clear': True})
 
 @app.route('/pixel/<int:pixel_index>/<string:pixel_colour>', methods=['GET'])
 def set_single_pixel(pixel_index,pixel_colour):
@@ -104,4 +149,4 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=flask_port)
+    app.run(debug=True, host='0.0.0.0',port=FLASK_PORT)
