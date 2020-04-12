bb721bbe30c1e5ff4e8d7ba9baa7a568bf6ea90c
Geek Boy <github@geekboy.com.au>
2020-04-12 23:39:52 +1000

    Changed Sensor class to not have base class of Thread

--- api_server/models/sensors/xc4438_microphone_sound.py
+++ api_server/models/sensors/xc4438_microphone_sound.py
@@ -1,35 +1,17 @@
 ############
-# This class reads analog input from a XC-4438 Microphone Sound sensor
-# which is connected to an MCP3008 analog A/D Converter
+# This class defines XC-4438 Microphone Sound sensor
 ############
 
 # Importing modules
-from time import sleep  # To add delay
 from models.sensors.sensor import Sensor
-from controllers.mcp3008.mcp3008_controller import Mcp3008Controller
 
 #### Define class ####
-class Xc4438Sensor():
-  stop_monitor=False
-
-  # This is the analog channel input number for MCP3008
-  MCP3008_CHANNEL=None
-
-  mcp3008_controller=None
-#  neopixel_controller = NeopixelController(18) #Todo 
-
-  is_monitor_running = False
-  is_neopixel_enabled = False
-
-  def __init__(self, sleep=0.05,max_volt=3.3,multiplier=100):
-    #super(Sensor, self).__init__()
+class Xc4438Sensor(Sensor):
+  def __init__(self, max_volt=3.3,multiplier=100):
+    super(Sensor, self).__init__()
     self.is_analog = True
 
     print("**** Created  xc4438_sensor ****")
-  #  self.mcp3008_controller = mcp3008_controller
-  #  self.MCP3008_CHANNEL=mcp3008_channel
-
-    self.TIME_TO_SLEEP = sleep
     self.MAX_INPUT_VOLTS = max_volt
     self.MULTIPLIER = multiplier
 
@@ -44,38 +26,3 @@ class Xc4438Sensor():
     level = ((data * self.MAX_INPUT_VOLTS * self.MULTIPLIER)/float(1023))
     level = round(level)
     return level
-
-  def run(self):
-    try:
-      print("**** Started Microphone Sound Sensor Monitor ****")
-
-      while True:
-        if (self.stop_monitor):
-          break
-        is_monitor_running=True
-        sound_output = self.mcp3008_controller.readAnalogInput(self.MCP3008_CHANNEL)
-        sound_volts = self.ConvertVolts(sound_output)
-        sound_level = self.ConvertSoundLevel(sound_output)
- 
-        if(self.DEBUG_MODE):
-          self.is_debug_message_printed = False
-          print("Sound : {} ({}V) {} bits".format(sound_level,sound_volts,sound_output))
-        elif(not self.is_debug_message_printed and not self.DEBUG_MODE):
-          self.is_debug_message_printed = True
-          print("****** Debug Off *****")
-
-        '''
-        if (self.is_neopixel_enabled):
-          if(sound_output > 100):
-            num_pixels=round(sound_output/100)
-            self.neopixel_controller.neopixel.rainbow_meter(num_pixels)
-          else:
-            self.neopixel_controller.neopixel.blank_neopixel()
-        '''
-        
-        sleep(self.TIME_TO_SLEEP)
-
-    # When ^C is used put colours back to none
-    except KeyboardInterrupt:
-      is_monitor_running=False
-      print("No more sound !!!!!")
