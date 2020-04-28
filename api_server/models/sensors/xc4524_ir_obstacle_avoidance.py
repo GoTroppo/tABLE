c8aa8b682d69655baf9a3a0511d174e0f79d811e
Geek Boy <github@geekboy.com.au>
2020-04-28 22:26:55 +1000

    Fixed Type of Sensor to be Non Analog

--- api_server/models/sensors/xc4524_ir_obstacle_avoidance.py
+++ api_server/models/sensors/xc4524_ir_obstacle_avoidance.py
@@ -1,22 +1,22 @@
-############
-# This class defines XC-4524 IR Obstacle Avoidance sensor
-# Reference:
-#   https://www.jaycar.com.au/arduino-compatible-ir-obstacle-avoidance-sensor-module/p/XC4524
-############
-
 # Importing modules
-from models.sensors.sensor import Sensor
-from models.devices.neopixel import Neopixel
+from . sensor import Sensor
+from . models.devices.neopixel import Neopixel
 
 #### Define class ####
 class Xc4524Sensor(Sensor):
+  '''
+  This class defines XC-4524 IR Obstacle Avoidance sensor
+  Reference:
+  - https://www.jaycar.com.au/arduino-compatible-ir-obstacle-avoidance-sensor-module/p/XC4524
+  '''
+
   neopixel_controller = None
   last_reading = None
 
   def __init__(self, max_volt=3.3,multiplier=100):
     super(Xc4524Sensor, self).__init__()
 
-    self.is_analog = True
+    self.is_analog = False
     print("**** Created  xc4524_sensor ****")
     self.MAX_INPUT_VOLTS = max_volt
     self.MULTIPLIER = multiplier
